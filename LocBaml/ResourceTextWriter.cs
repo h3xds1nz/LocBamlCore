b0a4cb8e940890435daaf364524a61a1e3056677
h3xds1nz <gamingshard@protonmail.com>
2024-10-05 20:51:34 +0200

    Optimize ResourceTextWriter, use SearchValues<char> for delimiter search

--- LocBaml/ResourceTextWriter.cs
+++ LocBaml/ResourceTextWriter.cs
@@ -2,90 +2,87 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-//---------------------------------------------------------------------------
-//
-// Description: ResourceTextWriter class 
-//              It writes values to a CSV file or tab-separated TXT file
-//
-//---------------------------------------------------------------------------
+// Modified 5th Oct 2024
+// by h3xds1nz
 
-using System;
-using System.IO;
+using System.Buffers;
 using System.Text;
-using System.Resources;
-using System.Collections;
-using System.Globalization;
-using System.Diagnostics;
+using System.IO;
+using System;
 
 namespace BamlLocalization
-{    
+{
     /// <summary>
-    /// the class that writes to a text file either tab delimited or comma delimited. 
+    /// ResourceTextWriter that writes values to a CSV file or tab-separated TXT file.
     /// </summary>
     internal sealed class ResourceTextWriter : IDisposable
     {
-        //-------------------------------
-        // constructor 
-        //-------------------------------
+        private readonly SearchValues<char> _delimiters;
+        private readonly TextWriter _writer;
+        private readonly char _delimiter;
+
+        private bool _firstColumn;
+
+        /// <summary>
+        /// Creates a <see cref="ResourceTextWriter"/> for the specified <paramref name="fileType"/>, with a destination specified by <paramref name="output"/>.
+        /// </summary>
+        /// <param name="fileType"></param>
+        /// <param name="output"></param>
         internal ResourceTextWriter(FileType fileType, Stream output)
         {
-            
+            ArgumentNullException.ThrowIfNull(output, nameof(output));
+
             _delimiter = LocBamlConst.GetDelimiter(fileType);
+            _delimiters = SearchValues.Create('\"', '\r', '\n', _delimiter);
 
-            if (output == null)
-                throw new ArgumentNullException("output");
-
-            // show utf8 byte order marker
-            UTF8Encoding encoding = new UTF8Encoding(true);           
-            _writer      = new StreamWriter(output, encoding);
+            // Append UTF8 BOM (Byte Order Marker)    
+            _writer = new StreamWriter(output, new UTF8Encoding(true));
             _firstColumn = true;
-       }
+        }
 
-       #region internal methods
-       //-----------------------------------
-       // Internal methods
-        //-----------------------------------
         internal void WriteColumn(string value)
-        {    
-            if (value == null)
-                    value = string.Empty;
+        {
+            // NULL means we will write string.Empty
+            // TODO: We could just skip this function then
+            ReadOnlySpan<char> valueSpan = value;
 
             // if it contains delimeter, quote, newline, we need to escape them
-            if (value.IndexOfAny(new char[]{'\"', '\r', '\n', _delimiter}) >= 0)
+            if (valueSpan.ContainsAny(_delimiters))
             {
                 // make a string builder at the minimum required length;
-                StringBuilder builder = new StringBuilder(value.Length + 2);
+                StringBuilder builder = new(valueSpan.Length + 2);
 
                 // put in the opening quote
                 builder.Append('\"');
-                
+
                 // double quote each quote
-                for (int i = 0; i < value.Length; i++)
+                for (int i = 0; i < valueSpan.Length; i++)
                 {
-                    builder.Append(value[i]);
-                    if (value[i] == '\"')
+                    builder.Append(valueSpan[i]);
+                    if (valueSpan[i] == '\"')
                     {
                         builder.Append('\"');
-                    }                       
+                    }
                 }
 
                 // put in the closing quote
                 builder.Append('\"');
-                value = builder.ToString();
+                // TODO: If we would write here, we avoid this ToString()
+                valueSpan = builder.ToString();
             }
 
             if (!_firstColumn)
             {
                 // if we are not the first column, we write delimeter
                 // to seperate the new cell from the previous ones.
-                _writer.Write(_delimiter);                
+                _writer.Write(_delimiter);
             }
             else
             {
-                _firstColumn = false;   // set false
+                _firstColumn = false;
             }
 
-            _writer.Write(value);            
+            _writer.Write(valueSpan);
         }
 
         internal void EndLine()
@@ -96,31 +93,12 @@ namespace BamlLocalization
             // set first column to true    
             _firstColumn = true;
         }
-        internal void Close()
-        {
-            if (_writer != null)
-            {
-                _writer.Close();
-            }
-        }
-       #endregion 
 
-        void IDisposable.Dispose()
-        {
-            Close();
-        }
+        // This will be called from IDisposable.Dispose()
+        internal void Close() => _writer?.Close();
 
-        
-        #region private members
-        private char        _delimiter;
-        private TextWriter  _writer;        
-        private bool        _firstColumn;
-        #endregion
-    }    
+        // IDisposable interface implementation
+        void IDisposable.Dispose() => Close();
+
+    }
 }
-
-
-
-
-
-    
