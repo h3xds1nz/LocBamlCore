86b37d459bc66504aa99975058a61e19d2e51b31
h3xds1nz <gamingshard@protonmail.com>
2024-10-05 20:14:20 +0200

    Rewrite StringLoader/ResourceManager to retrieve localized messages

--- LocBaml/StringLoader.cs
+++ LocBaml/StringLoader.cs
@@ -2,33 +2,36 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-//-------------------------------------------------------------------------------
-// Description: String Loader class, loading strings from StringTable Resource. 
-//-------------------------------------------------------------------------------
+// Modified 5th Oct 2024
+// by h3xds1nz
 
-
-using System;
-using System.Resources;
 using System.Globalization;
+using System.Resources;
+using System;
 
-namespace BamlLocalization 
+namespace BamlLocalization
 {
-    internal class StringLoader
+    /// <summary>
+    /// Encapsulates <see cref="ResourceManager"/> for the resource string table.
+    /// </summary>
+    internal static class StringLoader
     {
-         public static string Get( string id, params object[] args)
-         {
-             string message = _resourceManager.GetString(id);
-             if (message != null)
-             {
-                 // Apply arguments to formatted string (if applicable)
-                 if (args != null && args.Length > 0)
-                 {
-                     message = String.Format(CultureInfo.CurrentCulture, message, args);
-                 }
-             }             
-             return message;
-         }
-         // Get exception string resources for current locale
-        private static ResourceManager _resourceManager = new ResourceManager("Resources.StringTable", typeof(StringLoader).Assembly);
-    }//endof class StringLoader    
-}//endof namespace
+        /// <summary>
+        /// Retrieves the exception string resources <see cref="ResourceManager"/> for current locale, currently only en-US is available.
+        /// </summary>
+        private static readonly ResourceManager _resourceManager = new("Resources.StringTable", typeof(StringLoader).Assembly);
+
+        /// <summary>
+        /// Retrieves the message using <paramref name="id"/> and formats in any <paramref name="args"/> using <see cref="CultureInfo.CurrentCulture"/>.
+        /// </summary>
+        /// <param name="id">The resource ID to retrieve from resources.</param>
+        /// <param name="args">Additional arguments that the message requires.</param>
+        /// <returns>The formatted resource message or <see langword="null"/> if the resource was not found.</returns>
+        public static string? Get(string id, params ReadOnlySpan<object?> args)
+        {
+            string? message = _resourceManager.GetString(id);
+
+            return message is not null && !args.IsEmpty ? string.Format(CultureInfo.CurrentCulture, message, args) : message;
+        }
+    }
+}
